4d2daa4f983648f9d6474f7c12de0a6761a26be1
ArkHiMed-tech <boss.trushchelev@mail.ru>
2023-01-23 17:00:21 +0200

    add camera

---
+++ camera.py
@@ -0,0 +1,90 @@
+import pygame
+from player import Player
+from level import Level
+from constants import *
+from typing import Literal
+
+class Camera:
+    def __init__(self, player:Player, level:Level, screen:pygame.display.Screen) -> None:
+        self.cx = 0  # x of center of camera
+        self.cy = 0  # y of center of camera
+        self.to_x = 0
+        self.to_y = 0
+        self.strength = 5  # frames to push camera to target, that more that slower
+        self.frames_skipped = 0
+        self.block_size = self.dft_block_size
+        self.multiplier = 1  # alternative way to set current block size
+        self.player = player
+        self.level = level
+        self.screen = screen
+        self.dft_block_size = self.level.block_size
+
+    def drawGrid(self):
+        for x in range(0, self.level.level_width):
+            for y in range(0, self.level.level_height):
+                color = LIGHT_BROWN
+                if self.player.is_visible(self.level.dungeon.grid, self.level.dungeon.grid[x][y]):
+                    self.level.dungeon.grid[x][y].visible = True
+                    if self.level.dungeon.grid[x][y].type == 'earth':
+                        color = LIGHT_BROWN
+                    elif self.level.dungeon.grid[x][y].type == 'floor':
+                        color = BROWN
+                    elif self.level.dungeon.grid[x][y].type == 'door':
+                        color = WHITE
+                    elif self.level.dungeon.grid[x][y].type == 'wall':
+                        color = GRAY
+
+                elif self.level.dungeon.grid[x][y].visible or self.level.dungeon.grid[x][y].explored:
+                    self.level.dungeon.grid[x][y].explored = True
+                    self.level.dungeon.grid[x][y].visible = False
+
+                    if self.level.dungeon.grid[x][y].type == 'earth':
+                        color = LIGHT_BROWN_FADED
+                    elif self.level.dungeon.grid[x][y].type == 'floor':
+                        color = BROWN_FADED
+                    elif self.level.dungeon.grid[x][y].type == 'door':
+                        color = WHITE_FADED
+                    elif self.level.dungeon.grid[x][y].type == 'wall':
+                        color = GRAY_FADED
+                else:
+                    color = BLACK
+
+                if self.player.pos[0] == x and self.player.pos[1] == y:
+                    color = RED
+
+                cell = pygame.Rect(int(x * self.block_size + self.tl_x), int(y * self.block_size + self.tl_y), self.block_size, self.block_size)
+                pygame.draw.rect(self.screen, color, cell)
+                if color not in [RED, BLACK]:
+                    pygame.draw.rect(self.screen, GRAY_BORDER, cell, 1)
+                
+                self._next_frame()  # call in the end
+
+    @property
+    def tl_x(self):
+        return self.cx - self.screen.get_surface().get_size()[0] // 2
+
+    @property
+    def tl_y(self):
+        return self.cy - self.screen.get_surface().get_size()[1] // 2
+
+    def _next_frame(self):
+        self.cx = self.cx + int((self.to_x - self.cx) / (self.strength - self.frames_skipped))
+        self.cy = self.cy + int((self.to_y - self.cy) / (self.strength - self.frames_skipped))
+        if self.frames_skipped != self.strength:
+            self.frames_skipped += 1
+    
+    def move_to(self, x, y, type:Literal['cell', 'point']='cell'):
+        if type == 'point':
+            self.to_x = x
+            self.to_y = y
+        else: 
+            self.to_x = int(self.block_size * x + self.block_size / 2)
+            self.to_y = int(self.block_size * y + self.block_size / 2)
+    
+    def set_size(self, num, type:Literal['points', 'multiplier']='points'):
+        if type == 'points':
+            self.block_size = num
+            self.multiplier = self.block_size / self.dft_block_size
+        else:
+            self.multiplier = num
+            self.block_size = int(self.dft_block_size * self.multiplier)
--- main.py
+++ main.py
@@ -5,22 +5,9 @@ import dungeonGenerator
 from player import Player
 from level import Level
 from tiles import WallTile, DoorTile, EarthTile, FloorTile, tiles_sprites, layers
+from camera import Camera
+from constants import *
 
-BLACK = (20, 20, 20)
-BLACK_FADED = (0, 0, 0)
-WHITE = (200, 200, 200)
-WHITE_FADED = (160, 160, 160)
-BROWN = (205, 120, 34)
-BROWN_FADED = (175, 90, 4)
-LIGHT_BROWN = (243, 163, 83)
-LIGHT_BROWN_FADED = (200, 120, 40)
-SOFT_BROWN = (126, 109, 91)
-SOFT_BROWN_FADED = (96, 79, 61)
-GRAY = (110, 110, 111)
-GRAY_FADED = (80, 80, 81)
-GRAY_BORDER = (130, 130, 131)
-
-RED = (255, 0, 0)
 SCREEN = None
 
 
@@ -40,8 +27,9 @@ def main():
     player.weapon = {'damage': (8, 10), 'isDoubleHand': False, 'name': 'shortSword'}
     player.armor = {'defence': (0, 2), 'name': 'leatherArmor'}
     print(player.pos)
+    camera = Camera(player, level, SCREEN)
     while True:
-        drawGrid(player, level, block_size)
+        camera.drawGrid()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -81,43 +69,7 @@ def main():
         pygame.display.update()
 
 
-def drawGrid(player:Player, level:Level, blockSize: int = 20):
-    for x in range(0, level.level_width):
-        for y in range(0, level.level_height):
-            color = LIGHT_BROWN
-            if player.is_visible(level.dungeon.grid, level.dungeon.grid[x][y]):
-                level.dungeon.grid[x][y].visible = True
-                if level.dungeon.grid[x][y].type == 'earth':
-                    color = LIGHT_BROWN
-                elif level.dungeon.grid[x][y].type == 'floor':
-                    color = BROWN
-                elif level.dungeon.grid[x][y].type == 'door':
-                    color = WHITE
-                elif level.dungeon.grid[x][y].type == 'wall':
-                    color = GRAY
 
-            elif level.dungeon.grid[x][y].visible or level.dungeon.grid[x][y].explored:
-                level.dungeon.grid[x][y].explored = True
-                level.dungeon.grid[x][y].visible = False
-
-                if level.dungeon.grid[x][y].type == 'earth':
-                    color = LIGHT_BROWN_FADED
-                elif level.dungeon.grid[x][y].type == 'floor':
-                    color = BROWN_FADED
-                elif level.dungeon.grid[x][y].type == 'door':
-                    color = WHITE_FADED
-                elif level.dungeon.grid[x][y].type == 'wall':
-                    color = GRAY_FADED
-            else:
-                color = BLACK
-
-            if player.pos[0] == x and player.pos[1] == y:
-                color = RED
-
-            cell = pygame.Rect(x * blockSize, y * blockSize, blockSize, blockSize)
-            pygame.draw.rect(SCREEN, color, cell)
-            if color not in [RED, BLACK]:
-                pygame.draw.rect(SCREEN, GRAY_BORDER, cell, 1)
 
 
 if __name__ == "__main__":

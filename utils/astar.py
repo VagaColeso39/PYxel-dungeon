3d121ab3c8462fa9866cf40e769344b051acbff4
ArkHiMed-tech <boss.trushchelev@mail.ru>
2023-02-07 17:39:32 +0200

    push

--- utils/astar.py
+++ utils/astar.py
@@ -36,7 +36,7 @@ def return_path(current_node):
     return path[::-1]  # Return reversed path
 
 
-def astar(maze, start, end, allow_diagonal_movement = True):
+def astar(maze, start, end, allow_diagonal_movement = False):
     # Create start and end node
     start_node = Node(None, start)
     start_node.g = start_node.h = start_node.f = 0
